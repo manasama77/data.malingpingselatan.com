13efc5401fdc51de95e2893fb76bb7bd3155de58
Adam PM <adam.pm77@gmail.com>
2022-04-06 13:17:25 +0700

    update print header

---
+++ pages/_partials/print_header.php
@@ -0,0 +1,16 @@
+<div class="row">
+    <div class="col-12">
+        <div class="col-12" style="height: 120px;">
+            <img src="../../assets/img/logo.png" class="float-left" style="height: 130px;" />
+            <h4 style="top: 0; position: absolute; width:100%;" class="text-center">
+                PEMERINTAH KABUPATEN LEBAK<br />
+                KECAMATAN MALINGPING<br />
+                DESA MALINGPING SELATAN<br />
+                <p style="line-height: 14pt; font-size: 14px; margin-top: 10px;">
+                    Alamat : Jalan Lebak Lame Desa Malingping Selatan Kecamatan Malingping 42391<br />
+                    e-mail : malingpingselatan2018@gmail.com
+                </p>
+            </h4>
+        </div>
+    </div>
+</div>
--- pages/peristiwa-kerja-luar_negeri-atau-kota/print.php
+++ pages/peristiwa-kerja-luar_negeri-atau-kota/print.php
@@ -66,22 +66,7 @@ if (mysqli_num_rows($query_warga) == 0) {
     <div class="container">
         <div class="row">
             <div class="col-12">
-                <div class="row">
-                    <div class="col-12">
-                        <div class="col-12" style="height: 120px;">
-                            <img src="../../assets/img/logo.png" class="float-left" style="height: 130px;" />
-                            <h4 style="top: 0; position: absolute; width:100%;" class="text-center">
-                                PEMERINTAH KABUPATEN LEBAK<br />
-                                KECAMATAN MALINGPING<br />
-                                DESA MALINGPING SELATAN<br />
-                                <p style="line-height: 14pt; font-size: 14px; margin-top: 10px;">
-                                    Alamat : Jalan Lebak Lame Desa Malingping Selatan Kecamatan Malingping 42391<br />
-                                    e-mail : malingpingselatan2018@gmail.com
-                                </p>
-                            </h4>
-                        </div>
-                    </div>
-                </div>
+                <?php include('../_partials/print_header.php'); ?>
                 <hr style="border-top: 5px solid black;" />
                 <table class="table table-borderless table-condensed table-sm w-100 p-0">
                     <tbody>
