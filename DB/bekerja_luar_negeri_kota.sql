d9a841c0714977cc786bb9dd7f8a0fcca052992c
Adam PM <adam.pm77@gmail.com>
2022-04-07 21:38:49 +0700

    bekerja di luar negeri

--- DB/bekerja_luar_negeri_kota.sql
+++ DB/bekerja_luar_negeri_kota.sql
@@ -11,7 +11,7 @@
  Target Server Version : 100421
  File Encoding         : 65001
 
- Date: 28/03/2022 11:32:56
+ Date: 07/04/2022 21:38:31
 */
 
 SET NAMES utf8mb4;
@@ -33,11 +33,10 @@ CREATE TABLE `bekerja_luar_negeri_kota`  (
   `tanggal_pembuatan` date NULL DEFAULT NULL,
   `nomor_surat` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NULL DEFAULT NULL,
   `sequence` int UNSIGNED NULL DEFAULT NULL,
+  `nama_ttd` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NULL DEFAULT NULL,
+  `jabatan_ttd` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NULL DEFAULT NULL,
+  `nomor_induk_ttd` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NULL DEFAULT NULL,
   PRIMARY KEY (`id`) USING BTREE
-) ENGINE = InnoDB AUTO_INCREMENT = 1 CHARACTER SET = utf8mb4 COLLATE = utf8mb4_general_ci ROW_FORMAT = Dynamic;
-
--- ----------------------------
--- Records of bekerja_luar_negeri_kota
--- ----------------------------
+) ENGINE = InnoDB AUTO_INCREMENT = 2 CHARACTER SET = utf8mb4 COLLATE = utf8mb4_general_ci ROW_FORMAT = Dynamic;
 
 SET FOREIGN_KEY_CHECKS = 1;
