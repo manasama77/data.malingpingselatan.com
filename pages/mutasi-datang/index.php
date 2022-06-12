3d64d02b348a020881fed884635adefda8db5877
Adam PM <adam.pm77@gmail.com>
2022-06-12 12:18:32 +0700

    update 2022-06-12

--- pages/mutasi-datang/index.php
+++ pages/mutasi-datang/index.php
@@ -1,87 +1,324 @@
 <?php include('../_partials/top.php') ?>
+<?php
+include('_partials/menu.php');
+include('data-index.php');
+include('../dasbor/data-index.php');
+include('./_partials/Helper.php');
+unset_session_form();
+?>
 
 <h1 class="page-header">Data Mutasi Datang</h1>
-<?php include('_partials/menu.php') ?>
 
-<?php include('data-index.php') ?>
-<?php include('../dasbor/data-index.php') ?>
-
+<?php
+if (isset($_SESSION['success'])) {
+?>
+	<div class="alert alert-success alert-dismissible" role="alert">
+		<button type="button" class="close" data-dismiss="alert" aria-label="Close"><span aria-hidden="true">&times;</span></button>
+		<strong><?= $_SESSION['success']; ?></strong>
+	</div>
+<?php
+}
+unset($_SESSION['success']);
+?>
+<?php
+if (isset($_SESSION['warning'])) {
+?>
+	<div class="alert alert-warning alert-dismissible" role="alert">
+		<button type="button" class="close" data-dismiss="alert" aria-label="Close"><span aria-hidden="true">&times;</span></button>
+		<strong><?= $_SESSION['warning']; ?></strong>
+	</div>
+<?php
+}
+unset($_SESSION['warning']);
+?>
 <table class="table table-striped table-condensed table-hover" id="datatable">
-  <thead>
-    <tr>
-      <th>#</th>
-      <th>NIK</th>
-      <th>Nama Mutasi</th>
-      <th>L/P</th>
-      <!-- <th>Lahir</th>
-      <th>Usia</th>
-      <th>Pendidikan</th>
-      <th>Pekerjaan</th>
-      <th>Kawin</th> -->
-      <th>Status</th>
-      <th>Aksi</th>
-    </tr>
-  </thead>
-  <tbody>
-    <?php $nomor = 1; ?>
-    <?php foreach ($data_mutasi as $mutasi) : ?>
-    <tr>
-      <td><?php echo $nomor++ ?>.</td>
-      <td><?php echo $mutasi['nik_warga'] ?></td>
-      <td><?php echo $mutasi['nama_warga'] ?></td>
-      <td><?php echo $mutasi['jenis_kelamin_warga'] ?></td>
-      <!-- <td>
-        <?php echo ($mutasi['tanggal_lahir_mutasi'] != '0000-00-00') ? date('d-m-Y', strtotime($mutasi['tanggal_lahir_mutasi'])) : ''?>
-      </td> -->
-      <td><?php echo $mutasi['status_warga'] ?></td>
-      <td>
-        <!-- Single button -->
-        <div class="btn-group pull-right">
-          <button type="button" class="btn btn-default btn-xs dropdown-toggle" data-toggle="dropdown" aria-expanded="true">
-          <span class="caret"></span>
-          </button>
-          <ul class="dropdown-menu pull-right" role="menu">
-            <li>
-              <a href="show.php?id_mutasi_masuk=<?php echo $mutasi['id_mutasi_masuk'] ?>"><i class="glyphicon glyphicon-sunglasses"></i> Detail</a>
-            </li>
-            <li>
-              <a href="cetak-show.php?id_mutasi_masuk=<?php echo $mutasi['id_mutasi_masuk'] ?>" target="_blank"><i class="glyphicon glyphicon-print"></i> Cetak</a>
-            </li>
-            <?php if ($_SESSION['user']['status_user'] != 'RW'): ?>
-            <li class="divider"></li>
-            <li>
-              <a href="delete.php?id_mutasi_masuk=<?php echo $mutasi['id_mutasi_masuk'] ?>" onclick="return confirm('Yakin hapus data ini?')">
-                <i class="glyphicon glyphicon-trash"></i> Hapus
-              </a>
-            </li>
-            <?php endif; ?>
-          </ul>
-        </div>
-      </td>
-    </tr>
-    <?php endforeach ?>
-  </tbody>
+	<thead>
+		<tr>
+			<th>#</th>
+			<th>NIK</th>
+			<th>Nama Warga</th>
+			<th>L/P</th>
+			<th>Tgl Lahir</th>
+			<th>Tanggal Mutasi</th>
+			<th>Jenis Mutasi</th>
+			<th>Alasan Mutasi</th>
+			<th>Aksi</th>
+		</tr>
+	</thead>
+	<tbody>
+		<?php $nomor = 1; ?>
+		<?php foreach ($data_mutasi as $mutasi) : ?>
+			<tr>
+				<td><?php echo $nomor++ ?>.</td>
+				<td><?php echo $mutasi['nik_warga'] ?></td>
+				<td><?php echo $mutasi['nama_warga'] ?></td>
+				<td><?php echo ($mutasi['jenis_kelamin_warga'] == "L") ? "Laki-laki" : "Perempuan"; ?></td>
+				<td>
+					<?php
+					if ($mutasi['tanggal_lahir_warga'] != '0000-00-00') {
+						$tgl_lahir_obj = new DateTime($mutasi['tanggal_lahir_warga']);
+						echo $tgl_lahir_obj->format('d-M-Y');
+					}
+					?>
+				</td>
+				<td>
+					<?php
+					if ($mutasi['tanggal_pindah'] != '0000-00-00') {
+						$tgl_pindah_obj = new DateTime($mutasi['tanggal_pindah']);
+						echo $tgl_pindah_obj->format('d-M-Y');
+					}
+					?>
+				</td>
+				<td><?php echo $mutasi['jenis_kepindahan'] ?></td>
+				<td><?php echo $mutasi['alasan_pindah'] ?></td>
+				<td>
+					<div class="btn-group pull-right">
+						<button type="button" class="btn btn-default btn-xs dropdown-toggle" data-toggle="dropdown" aria-expanded="true">
+							<span class="caret"></span>
+						</button>
+						<ul class="dropdown-menu pull-right" role="menu">
+							<li>
+								<!-- <a href="show.php?id_mutasi_masuk=<?php echo $mutasi['id_mutasi_masuk'] ?>"><i class="glyphicon glyphicon-sunglasses"></i> Detail</a> -->
+								<a href="#" class="btn_detail" data-id="<?= $mutasi['id_mutasi_masuk'] ?>"><i class="glyphicon glyphicon-sunglasses"></i> Detail</a>
+							</li>
+							<li>
+								<a href="cetak-show.php?id_mutasi_masuk=<?php echo $mutasi['id_mutasi_masuk'] ?>" target="_blank"><i class="glyphicon glyphicon-print"></i> Cetak</a>
+							</li>
+							<?php if ($_SESSION['user']['status_user'] != 'RW') : ?>
+								<li class="divider"></li>
+								<li>
+									<a href="delete.php?id_mutasi_masuk=<?php echo $mutasi['id_mutasi_masuk'] ?>" onclick="return confirm('Yakin hapus data ini?')">
+										<i class="glyphicon glyphicon-trash"></i> Hapus
+									</a>
+								</li>
+							<?php endif; ?>
+						</ul>
+					</div>
+				</td>
+			</tr>
+		<?php endforeach ?>
+	</tbody>
 </table>
 
 <br><br>
 
 <div class="well">
-  <dl class="dl-horizontal">
-    <dt>Total Mutasi</dt>
-    <dd><?php echo $jumlah_mutasi_masuk['total'] ?> orang</dd>
+	<dl class="dl-horizontal">
+		<dt>Total Mutasi</dt>
+		<dd><?php echo $jumlah_mutasi_masuk['total'] ?> orang</dd>
 
-    <dt>Jumlah Laki-laki</dt>
-    <dd><?php echo $jumlah_mutasi_masuk_l['total'] ?> orang</dd>
+		<dt>Jumlah Laki-laki</dt>
+		<dd><?php echo $jumlah_mutasi_masuk_l['total'] ?> orang</dd>
 
-    <dt>Jumlah Perempuan</dt>
-    <dd><?php echo $jumlah_mutasi_masuk_p['total'] ?> orang</dd>
+		<dt>Jumlah Perempuan</dt>
+		<dd><?php echo $jumlah_mutasi_masuk_p['total'] ?> orang</dd>
 
-    <dt>Warga < 17 tahun</dt>
-    <dd><?php echo $jumlah_mutasi_masuk_kd_17['total'] ?> orang</dd>
+		<dt>Warga < 17 tahun</dt>
+		<dd><?php echo $jumlah_mutasi_masuk_kd_17['total'] ?> orang</dd>
 
-    <dt>Warga >= 17 tahun</dt>
-    <dd><?php echo $jumlah_mutasi_masuk_ld_17['total'] ?> orang</dd>
-  </dl>
+		<dt>Warga >= 17 tahun</dt>
+		<dd><?php echo $jumlah_mutasi_masuk_ld_17['total'] ?> orang</dd>
+	</dl>
 </div>
 
+<!-- Modal -->
+<div class="modal fade" id="modal_detail" tabindex="-1" role="dialog" aria-labelledby="myModalLabel">
+	<div class="modal-dialog modal-lg" role="document">
+		<div class="modal-content">
+			<div class="modal-header">
+				<button type="button" class="close" data-dismiss="modal" aria-label="Close"><span aria-hidden="true">&times;</span></button>
+				<h4 class="modal-title" id="myModalLabel">Detail Mutasi Datang</h4>
+			</div>
+			<div class="modal-body" id="v_detail">
+			</div>
+			<div class="modal-footer">
+				<button type="button" class="btn btn-default" data-dismiss="modal">Close</button>
+			</div>
+		</div>
+	</div>
+</div>
 <?php include('../_partials/bottom.php') ?>
+
+<script src="https://cdnjs.cloudflare.com/ajax/libs/moment.js/2.29.3/moment.min.js" integrity="sha512-x/vqovXY/Q4b+rNjgiheBsA/vbWA3IVvsS8lkQSX1gQ4ggSJx38oI2vREZXpTzhAv6tNUaX81E7QBBzkpDQayA==" crossorigin="anonymous" referrerpolicy="no-referrer"></script>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/moment.js/2.29.3/moment-with-locales.min.js" integrity="sha512-vFABRuf5oGUaztndx4KoAEUVQnOvAIFs59y4tO0DILGWhQiFnFHiR+ZJfxLDyJlXgeut9Z07Svuvm+1Jv89w5g==" crossorigin="anonymous" referrerpolicy="no-referrer"></script>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/moment.js/2.29.3/locale/id.min.js" integrity="sha512-he8U4ic6kf3kustvJfiERUpojM8barHoz0WYpAUDWQVn61efpm3aVAD8RWL8OloaDDzMZ1gZiubF9OSdYBqHfQ==" crossorigin="anonymous" referrerpolicy="no-referrer"></script>
+<script>
+	$(document).ready(function() {
+		$('.btn_detail').on('click', function(e) {
+			e.preventDefault()
+			showDetail($(this).data('id'))
+		})
+	})
+
+	function showDetail(id) {
+		$.ajax({
+			url: `data-show.php`,
+			method: 'get',
+			dataType: 'json',
+			data: {
+				id_mutasi_masuk: id
+			}
+		}).fail(e => {
+			console.log(e.responseText)
+		}).done(e => {
+			if (e.code == 400) {
+				alert(`${e.message}`)
+			} else if (e.code == 200) {
+				let htmlnya = ``
+
+				htmlnya += `
+					<table class="table table-striped table-middle">
+						<tr>
+							<th width="20%">Jenis Kepindahan</th>
+							<td width="1%">:</td>
+							<td id="jenis_kepindahan">${e.data.jenis_kepindahan}</td>
+						</tr>
+					</table>
+				`
+
+				let group_kepala = ``
+				let group_anggota = ``
+				if (e.data.jenis_kepindahan == "Kepala Keluarga") {
+					group_kepala = `
+						<tr>
+							<th width="20%">Nomor Kartu Keluarga</th>
+							<td width="1%">:</td>
+							<td id="nomor_keluarga">${e.data.nomor_keluarga}</td>
+						</tr>
+					`
+				} else if (e.data.jenis_kepindahan == "Anggota Keluarga") {
+					group_anggota = `
+						<tr>
+							<th width="20%">Nomor Kartu Keluarga</th>
+							<td width="1%">:</td>
+							<td id="nomor_keluarga">${e.data.nomor_keluarga}</td>
+						</tr>
+						<tr>
+							<th width="20%">Kepala Keluarga</th>
+							<td width="1%">:</td>
+							<td id="nomor_keluarga">(${e.data.nik_kepala_keluarga}) ${e.data.nama_kepala_keluarga}</td>
+						</tr>
+					`
+				}
+
+				htmlnya += `
+					<h3>Data Pribadi</h3>
+					<table class="table table-striped table-middle">
+						${group_kepala}
+						${group_anggota}
+						<tr>
+							<th width="20%">NIK Warga</th>
+							<td width="1%">:</td>
+							<td id="nik_kepala_keluarga">${e.data.nik_warga}</td>
+						</tr>
+						<tr>
+							<th>Nama Warga</th>
+							<td>:</td>
+							<td id="nama_kepala_keluarga">${e.data.nama_warga}</td>
+						</tr>
+						<tr>
+							<th>Tempat Lahir</th>
+							<td>:</td>
+							<td id="tempat_lahir_warga">${e.data.tempat_lahir_warga}</td>
+						</tr>
+						<tr>
+							<th>Tanggal Lahir</th>
+							<td>:</td>
+							<td id="tanggal_lahir_warga">${moment(e.data.tanggal_lahir_warga).format('DD MMMM YYYY')}</td>
+						</tr>
+						<tr>
+							<th>Jenis Kelamin</th>
+							<td>:</td>
+							<td id="jk">${(e.data.jenis_kelamin_warga == "L") ? "Laki-laki" : "Perempuan"}</td>
+						</tr>
+						<tr>
+							<th>Agama</th>
+							<td>:</td>
+							<td id="agama_warga">${e.data.agama_warga}</td>
+						</tr>
+						<tr>
+							<th>Pendidikan Terakhir</th>
+							<td>:</td>
+							<td id="pendidikan_terakhir_warga">${e.data.pendidikan_terakhir_warga}</td>
+						</tr>
+						<tr>
+							<th>Pekerjaan</th>
+							<td>:</td>
+							<td id="pekerjaan_warga">${e.data.pekerjaan_warga}</td>
+						</tr>
+					</table>
+
+					<h3>Data Daerah Tujuan</h3>
+					<table class="table table-striped table-middle">
+						<tr>
+							<th width="20%" style="vertical-align: top !important;">Alamat Tujuan</th>
+							<td width="1%" style="vertical-align: top !important;">:</td>
+							<td id="alamat_asal" style="vertical-align: top !important;">
+								${e.data.alamat_tujuan.replace(/(?:\r\n|\r|\n)/g, '<br>')}
+							</td>
+						</tr>
+						<tr>
+							<th width="20%">RT</th>
+							<td width="1%">:</td>
+							<td id="rt_warga">${e.data.rt_warga}</td>
+						</tr>
+						<tr>
+							<th>RW</th>
+							<td>:</td>
+							<td id="rw_warga">${e.data.rw_warga}</td>
+						</tr>
+						<tr>
+							<th>Desa/Kelurahan</th>
+							<td>:</td>
+							<td id="kelurahan">${e.data.desa_kelurahan_warga}</td>
+						</tr>
+						<tr>
+							<th>Kecamatan</th>
+							<td>:</td>
+							<td id="kecamatan">${e.data.kecamatan_warga}</td>
+						</tr>
+						<tr>
+							<th>Kabupaten/Kota</th>
+							<td>:</td>
+							<td id="kokab">${e.data.kabupaten_kota_warga}</td>
+						</tr>
+						<tr>
+							<th>Provinsi</th>
+							<td>:</td>
+							<td id="provinsi">${e.data.provinsi_warga}</td>
+						</tr>
+						<tr>
+							<th>Negara</th>
+							<td>:</td>
+							<td id="negara">${e.data.negara_warga}</td>
+						</tr>
+					</table>
+
+					<h3>Data Kepindahan</h3>
+					<table class="table table-striped table-middle">
+						<tr>
+							<th width="20%" style="vertical-align: top !important;">Alamat Asal</th>
+							<td width="1%" style="vertical-align: top !important;">:</td>
+							<td id="alamat_asal" style="vertical-align: top !important;">
+								${e.data.alamat_asal.replace(/(?:\r\n|\r|\n)/g, '<br>')}
+							</td>
+						</tr>
+						<tr>
+							<th>Tanggal Pindah</th>
+							<td>:</td>
+							<td id="tanggal_pindah">${moment(e.data.tanggal_pindah).format('DD MMMM YYYY')}</td>
+						</tr>
+						<tr>
+							<th>Alasan Pindah</th>
+							<td>:</td>
+							<td id="alasan_pindah">${e.data.alasan_pindah}</td>
+						</tr>
+					</table>
+			`
+
+				$('#v_detail').html(htmlnya)
+				$('#modal_detail').modal('show')
+			}
+		})
+	}
+</script>
--- pages/mutasi-datang/store_datang.php
+++ pages/mutasi-datang/store_datang.php
@@ -1,85 +1,82 @@
 <?php
 session_start();
 if (!isset($_SESSION['user'])) {
-  // jika user belum login
-  header('Location: ../login');
-  exit();
+	// jika user belum login
+	header('Location: ../login');
+	exit();
 }
 
 include('../../config/koneksi.php');
+include('./_partials/Helper.php');
 
-// ambil data dari form
-$id_warga = htmlspecialchars($_POST['id_warga']);
-$id_keluarga = htmlspecialchars($_POST['id_keluarga']);
-$nomor_keluarga = htmlspecialchars($_POST['nomor_keluarga']);
-$nama_kepala_keluarga = htmlspecialchars($_POST['nama_kepala_keluarga']);
-$tempat_lahir_warga = htmlspecialchars($_POST['tempat_lahir_warga']);
-$tanggal_lahir_warga = htmlspecialchars($_POST['tanggal_lahir_warga']);
-$jenis_kelamin_warga = htmlspecialchars($_POST['jk']);
-/*
-$alamat_warga = htmlspecialchars($_POST['alamat_warga']);
-$desa_kelurahan_warga = 'Dukuhdalem';
-$kecamatan_warga = 'Ciawi Gebang';
-$kabupaten_kota_warga = 'Kuningan';
-$provinsi_warga = 'Jawa Barat';
-$negara_warga = 'Indonesia';
+// CATCH POST REQUEST
+set_session_from_post();
 
-$agama_warga = htmlspecialchars($_POST['agama_warga']);
-$pendidikan_terakhir_warga = htmlspecialchars($_POST['pendidikan_terakhir_warga']);
-$pekerjaan_warga = htmlspecialchars($_POST['pekerjaan_warga']);
-$status_perkawinan_warga = htmlspecialchars($_POST['status_perkawinan_warga']);
-*/
-$dusun_warga = htmlspecialchars($_POST['dusun_warga']);
-$rt_warga = htmlspecialchars($_POST['rt_warga']);
-$rw_warga = htmlspecialchars($_POST['rw_warga']);
+if ($_SESSION['jenis_kepindahan'] == "Kepala Keluarga") {
+	$sql_cek_kk      = "SELECT nomor_keluarga from kartu_keluarga where nomor_keluarga = '" . $_SESSION['nomor_keluarga'] . "'";
+	$query_cek_kk    = mysqli_query($db, $sql_cek_kk);
+	$num_rows_cek_kk = mysqli_num_rows($query_cek_kk);
+	if ($num_rows_cek_kk > 0) {
+		$_SESSION['warning']        = "Tambah warga gagal!, No. KK " . $_SESSION['nomor_keluarga'] . " sudah digunakan !";
+		header('location: pindah_masuk.php');
+		exit();
+	}
+}
+
+$sql_cek_nik      = "SELECT nik_warga from warga where nik_warga = '" . $_SESSION['nik_warga'] . "'";
+$query_cek_nik    = mysqli_query($db, $sql_cek_nik);
+$num_rows_cek_nik = mysqli_num_rows($query_cek_nik);
+if ($num_rows_cek_nik > 0) {
+	$_SESSION['warning'] = "Tambah warga gagal!, NIK " . $_SESSION['nik_warga'] . " sudah digunakan !";
+	header('location: pindah_masuk.php');
+	exit();
+}
+
+$id_user      = $_SESSION['user']['id_user'];
 $status_warga = 'Pindah Datang';
-$alamat_asal = htmlspecialchars($_POST['alamat_asal']);
-$tanggal_pindah = htmlspecialchars($_POST['tanggal_pindah']);
-$alasan_pindah = htmlspecialchars($_POST['alasan_pindah']);
-$jenis_kepindahan = htmlspecialchars($_POST['jenis_kepindahan']);
-$nik_sementara=uniqid();
 
-$id_user = $_SESSION['user']['id_user'];
-//cek nik warga dari database apakah ada atau tidak
 
-$query_cek="SELECT nomor_keluarga from kartu_keluarga where nomor_keluarga=$nomor_keluarga";
-$cek_nik=mysqli_num_rows(mysqli_query($db, $query_cek));
-if($cek_nik>0){
-  echo "<script>window.alert('Tambah warga gagal!, No. KK $nomor_keluarga sudah digunakan !'); history.back()</script>";
+$sql_warga = "
+	INSERT INTO warga 
+	(
+		nik_warga, nama_warga, tempat_lahir_warga, tanggal_lahir_warga, jenis_kelamin_warga, alamat_ktp_warga, alamat_warga, desa_kelurahan_warga, kecamatan_warga, kabupaten_kota_warga, provinsi_warga, negara_warga, dusun_warga, rt_warga, rw_warga, agama_warga, pendidikan_terakhir_warga, pekerjaan_warga, status_warga, id_user, 
+		created_at, updated_at
+	) 
+	VALUES 
+	(
+		'" . $_SESSION['nik_warga'] . "', '" . $_SESSION['nama_warga'] . "', '" . $_SESSION['tempat_lahir_warga'] . "', '" . $_SESSION['tanggal_lahir_warga'] . "', '" . $_SESSION['jenis_kelamin_warga'] . "', '" . $_SESSION['alamat_asal'] . "', '" . $_SESSION['alamat_tujuan'] . "', '" . $_SESSION['kelurahan'] . "', '" . $_SESSION['kecamatan'] . "', '" . $_SESSION['kokab'] . "', '" . $_SESSION['provinsi'] . "', '" . $_SESSION['negara'] . "', null, '" . $_SESSION['rt_warga'] . "', '" . $_SESSION['rw_warga'] . "', '" . $_SESSION['agama_warga'] . "', '" . $_SESSION['pendidikan_terakhir_warga'] . "', '" . $_SESSION['pekerjaan_warga'] . "', '" . $status_warga . "',  '" . $id_user . "', CURRENT_TIMESTAMP,  null
+	)
+";
+$query_warga = mysqli_query($db, $sql_warga);
+$id_warga    = mysqli_insert_id($db);
+
+if ($_SESSION['jenis_kepindahan'] == "Kepala Keluarga") {
+	$sql_kk = "INSERT INTO kartu_keluarga 
+		(nomor_keluarga, id_kepala_keluarga, alamat_keluarga, desa_kelurahan_keluarga, kecamatan_keluarga, kabupaten_kota_keluarga, provinsi_keluarga, negara_keluarga, rt_keluarga, rw_keluarga, kode_pos_keluarga, id_user, created_at, updated_at) 
+		VALUES ('" . $_SESSION['nomor_keluarga'] . "', '" . $id_warga . "', '" . $_SESSION['alamat_asal'] . "', '" . $_SESSION['kelurahan'] . "', '" . $_SESSION['kecamatan'] . "', '" . $_SESSION['kokab'] . "', '" . $_SESSION['provinsi'] . "', '" . $_SESSION['negara'] . "', '" . $_SESSION['rt_warga'] . "', '" . $_SESSION['rw_warga'] . "', '-', '" . $id_user . "', CURRENT_TIMESTAMP, null);";
+	$query_kk = mysqli_query($db, $sql_kk);
+	$id_keluarga = mysqli_insert_id($db);
+	$_SESSION['id_keluarga'] = $id_keluarga;
+} elseif ($_SESSION['jenis_kepindahan'] == "Anggota Keluarga") {
+	$sql_warga_has_kartu_keluarga   = "INSERT INTO warga_has_kartu_keluarga (id_warga, id_keluarga) VALUES (" . $id_warga . ", " . $_SESSION['id_keluarga'] . ")";
+	$query_warga_has_kartu_keluarga = mysqli_query($db, $sql_warga_has_kartu_keluarga);
+}
+
+$sql_mutasi = "INSERT INTO mutasi_masuk 
+	(id_warga, id_keluarga, alamat_asal, alamat_tujuan, tanggal_pindah, alasan_pindah, jenis_kepindahan, agama_warga, pendidikan_terakhir_warga, pekerjaan_warga, rt_warga, rw_warga, desa_kelurahan_warga, kecamatan_warga, kabupaten_kota_warga, provinsi_warga) 
+	VALUES
+	('" . $id_warga . "', '" . $_SESSION['id_keluarga'] . "', '" . $_SESSION['alamat_asal'] . "', '" . $_SESSION['alamat_tujuan'] . "', '" . $_SESSION['tanggal_pindah'] . "', '" . $_SESSION['alasan_pindah'] . "', '" . $_SESSION['jenis_kepindahan'] . "', '" . $_SESSION['agama_warga'] . "', '" . $_SESSION['pendidikan_terakhir_warga'] . "', '" . $_SESSION['pekerjaan_warga'] . "', '" . $_SESSION['rt_warga'] . "', '" . $_SESSION['rw_warga'] . "', '" . $_SESSION['kelurahan'] . "', '" . $_SESSION['kecamatan'] . "', '" . $_SESSION['kokab'] . "', '" . $_SESSION['provinsi'] . "' );";
+$query_mutasi = mysqli_query($db, $sql_mutasi);
+
+//cek keberhasilan pendambahan data
+if ($query_warga === TRUE && $query_mutasi === TRUE) {
+	unset_session_form();
+	$_SESSION['success'] = "Tambah Mutasi Masuk Warga Berhasil!";
+	header("Location: index.php");
+	exit();
 } else {
-  //echo "NIK belum digunakan :)";
-//}
-
-
-//echo $query_cek;
-// masukkan ke database
-
-$query_warga = "INSERT INTO warga (id_warga, nik_warga, nama_warga, tempat_lahir_warga, tanggal_lahir_warga, jenis_kelamin_warga, alamat_ktp_warga, alamat_warga, desa_kelurahan_warga, kecamatan_warga, kabupaten_kota_warga, provinsi_warga, negara_warga, dusun_warga, rt_warga, rw_warga, agama_warga, pendidikan_terakhir_warga, pekerjaan_warga, status_warga, id_user, created_at, updated_at) VALUES (NULL, '$nik_sementara', '$nama_kepala_keluarga', '$tempat_lahir_warga', '$tanggal_lahir_warga', '$jenis_kelamin_warga', '$alamat_asal', '$alamat_asal', '-', '-', '-', '-', '-', '$dusun_warga', '$rt_warga', '$rw_warga','-','-','-',
-	'$status_warga', '$id_user', CURRENT_TIMESTAMP, '0000-00-00 00:00:00.000000');";
-
-//echo $query;
-
-$query_kk= "INSERT INTO kartu_keluarga (id_keluarga, nomor_keluarga, id_kepala_keluarga, alamat_keluarga, desa_kelurahan_keluarga, kecamatan_keluarga, kabupaten_kota_keluarga, provinsi_keluarga, negara_keluarga, rt_keluarga, rw_keluarga, kode_pos_keluarga, id_user, created_at, updated_at) VALUES ($id_keluarga, '$nomor_keluarga', '$id_warga', '$alamat_asal', '-', '-', '-', '-', '-', '-', '-', '-', '$id_user', CURRENT_TIMESTAMP, '0000-00-00 00:00:00.000000');";
-
-  $query_mutasi = "INSERT INTO mutasi_masuk (id_mutasi_masuk,id_warga, id_keluarga, dusun_masuk,rt_masuk,rw_masuk,alamat_asal,tanggal_pindah,alasan_pindah,jenis_kepindahan) VALUES(NULL, '$id_warga','$id_keluarga','$dusun_warga','$rt_warga','$rw_warga','$alamat_asal','$tanggal_pindah','$alasan_pindah','$jenis_kepindahan');";
-  
-echo "<br>".$query_warga."<br>";
-echo "<br>".$query_kk."<br>";
-echo "<br>".$query_mutasi."<br>";
-
-
-  $hasil_warga = mysqli_query($db, $query_warga);
-  $hasil_kk = mysqli_query($db, $query_kk);
-  $hasil = mysqli_query($db, $query_mutasi);
-
- //cek keberhasilan pendambahan data
-  if ($hasil_kk == true) {
-     echo "<script>window.alert('Tambah mutasi berhasil'); window.location.href='../mutasi-datang/index.php'</script>";
-     # echo "<script>window.alert('Tambah mutasi berhasil');</script>";
-  } else {
-      echo "<script>window.alert('Tambah mutasi gagal!'); history.back()</script>";
-     #  echo "<script>window.alert('Tambah mutasi gagal!');</script>";
-      #echo mysqli_error($db);
-  }
-
+	unset_session_form();
+	$_SESSION['warning'] = "Tambah Mutasi Masuk Warga Gagal!";
+	header("Location: index.php");
+	exit();
 }
