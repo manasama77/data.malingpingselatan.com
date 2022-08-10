4a693f70a87ed7d91318d33442daab9c917f4b15
Adam PM <adam.pm77@gmail.com>
2022-08-10 20:21:27 +0700

    add captcha

--- pages/login/index.php
+++ pages/login/index.php
@@ -38,13 +38,17 @@ if (isset($_SESSION['user'])) {
               <div class="form-group">
                 <input class="form-control" placeholder="Kata Sandi" name="password_user" type="password" value="" required="">
               </div>
-              <!--
-                                <div class="checkbox">
-                                    <label>
-                                        <input name="remember" type="checkbox" value="Remember Me">Remember Me
-                                    </label>
-                                </div>
-                                <!-- Change this to a button or input when using this as a form -->
+              <div class="form-group">
+                <label for="password">Captcha</label>
+                <div class="row">
+                  <div class="col-sm-12 col-md-6">
+                    <input type="text" class="form-control" name="captcha" id="captcha" minlength="4" maxlength="4" required />
+                  </div>
+                  <div class="col-sm-12 col-md-6">
+                    <img src="../../assets/captcha.php" alt="PHP Captcha">
+                  </div>
+                </div>
+              </div>
               <button type="submit" class="btn btn-lg btn-primary btn-block">Masuk </button>
             </fieldset>
           </form>
@@ -52,30 +56,6 @@ if (isset($_SESSION['user'])) {
       </div>
     </div>
   </div>
-  </div>
-
 </body>
 
-
-<!--
-<div class="row" style="margin-top: 75px">
-  <div class="col-md-4 col-md-offset-4">
-    <div class="well">
-
-      <form class="form-signin" method="post" action="../login/proses-login.php">
-        <h2 class="form-signin-heading text-center">
-          <strong>LOGIN</strong>          
-        </h2>        
-        <input type="text" name="username_user" class="form-control" placeholder="Username" autofocus required>
-
-        <input type="password" name="password_user" class="form-control" placeholder="Password" required>
-
-        <button class="btn btn-lg btn-primary btn-block" type="submit">
-          <i class="glyphicon glyphicon-log-in"></i> Log in
-        </button>
-      </form>
-    </div>
-  </div>
-</div>
--->
 <?php include('../_partials/bottom-login.php') ?>
