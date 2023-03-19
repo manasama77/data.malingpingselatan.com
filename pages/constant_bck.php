f11ca86d84ffe49e75646258bb4235a190213f64
Adam PM <adam.pm77@gmail.com>
2023-03-19 17:26:56 +0700

    Commit message

---
+++ pages/constant_bck.php
@@ -0,0 +1,20 @@
+<?php
+define("LOGO",  "logo-malingping.png");
+define("DESA",  "Malingping Selatan");
+define("KECAMATAN",  "Malingping");
+define("KOKAB",  "Kabupaten Lebak");
+define("PROVINSI",  "Banten");
+define("ALAMAT",  "Jalan Lebak Lame Desa Malingping Selatan Kecamatan Malingping 42391");
+define("EMAIL",  "malingpingselatan2018@gmail.com");
+define("KODE_DESA_SURAT",  "Pemdes.Mlp.Sel");
+define("PERWAKILAN",  "Kepala Desa Malingping Selatan");
+define("TTD_TITLE",  "a.n. Kepala Desa <br /> Kepala Urusan Umum");
+define("TTD_NAME", " M. AGUNG TAMARA R.<br /> NRPDes.198610202001062046");
+define("PRINT_KOKAB",  "PEMERINTAH KABUPATEN LEBAK");
+define("PRINT_KECAMATAN",  "KECAMATAN MALINGPING");
+define("PRINT_DESA",  "DESA MALINGPING SELATAN");
+define("INSTANSI",  "Pemerintah Desa Malingping Selatan");
+define("KODE_PROVINSI",  36);
+define("KODE_KOKAB",  02);
+define("KODE_KECAMATAN",  01);
+define("KODE_DESA",  2018);
